2fa2ac2ff319cf686f62fca4057984deaa4f6034
minjongkim <rlaalswhd200@naver.com>
2021-11-28 18:56:52 +0900

    Initial Commit

---
+++ RisingCamp_Week2_BurgerKing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingCamp_Week2_BurgerKing
+//
+//  Created by 김민종 on 2021/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
