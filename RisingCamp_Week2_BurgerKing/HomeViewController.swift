7a2ddfeb351b51bbf401055e3048b8b7802293ed
minjongkim <rlaalswhd200@naver.com>
2021-11-29 19:36:09 +0900

    added horizontal scrollView and uiimage

--- RisingCamp_Week2_BurgerKing/HomeViewController.swift
+++ RisingCamp_Week2_BurgerKing/HomeViewController.swift
@@ -7,26 +7,70 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UIScrollViewDelegate {
 
-     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+
+    @IBOutlet weak var imageBanner1: UIImageView!
+    @IBOutlet weak var imageBanner2: UIImageView!
+    
+    var stackView = UIStackView()
+    
+    var images: [String] = ["홈배너1", "홈배너2", "홈배너3", "홈배너4", "홈배너5", "홈배너6"]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pageControl.numberOfPages = images.count
+        for index in 0..<images.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imgView = UIImageView(frame: frame)
+            imgView.contentMode = .scaleAspectFit
+            imgView.image = UIImage(named: images[index])
+            self.scrollView.addSubview(imgView)
+        }
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(images.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
         
         
-        func viewDidAppear(animated: Bool) {
+        
+        imageBanner1.image = UIImage(named: "킹오더 배너")
+        imageBanner1.contentMode = .scaleAspectFit
+        
+        imageBanner2.image = UIImage(named: "딜리버리오토바이 배너")
+        imageBanner2.contentMode = .scaleAspectFit
 
-           let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 40))
-            imageView.contentMode = .scaleAspectFit
+        
+//        configureStackView()
 
-           let image = UIImage(named: "네비게이션 로고")
-           imageView.image = image
+    
+}
 
-            navigationItem.titleView = imageView
-         }
+
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
     }
-
-
+    
+//    func configureStackView() {
+//        view.addSubview(stackView)
+//        stackView.axis = .horizontal
+//        stackView.distribution = .fillEqually
+//        stackView.spacing = 10
+        
+        
+//    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
