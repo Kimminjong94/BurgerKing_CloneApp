0c10badb54fa8ece42e187401e4851d01aed6985
minjongkim <rlaalswhd200@naver.com>
2021-11-30 15:27:25 +0900

    Finished making home page

---
+++ RisingCamp_Week2_BurgerKing/CouponViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CouponView.swift
+//  RisingCamp_Week2_BurgerKing
+//
+//  Created by 김민종 on 2021/11/30.
+//
+
+import UIKit
+
+class CouponViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        
+    }
+    
+    @IBAction func navigationBanner1(_ sender: UIButton) {
+        
+
+    }
+    
+    @IBAction func navigationBanner2(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func navigationBanner3(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+}
--- RisingCamp_Week2_BurgerKing/HomeViewController.swift
+++ RisingCamp_Week2_BurgerKing/HomeViewController.swift
@@ -11,11 +11,11 @@ class HomeViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
-
-    @IBOutlet weak var imageBanner1: UIImageView!
-    @IBOutlet weak var imageBanner2: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
     
-    var stackView = UIStackView()
+//    @IBOutlet weak var imageBanner1: UIImageView!
+//    @IBOutlet weak var imageBanner2: UIImageView!
+    
     
     var images: [String] = ["홈배너1", "홈배너2", "홈배너3", "홈배너4", "홈배너5", "홈배너6"]
     var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
@@ -38,17 +38,52 @@ class HomeViewController: UIViewController, UIScrollViewDelegate {
         
         
         
-        imageBanner1.image = UIImage(named: "킹오더 배너")
-        imageBanner1.contentMode = .scaleAspectFit
-        
-        imageBanner2.image = UIImage(named: "딜리버리오토바이 배너")
-        imageBanner2.contentMode = .scaleAspectFit
+//        imageBanner1.image = UIImage(named: "킹오더 배너")
+//        imageBanner1.contentMode = .scaleAspectFit
+//
+//        imageBanner2.image = UIImage(named: "딜리버리오토바이 배너")
+//        imageBanner2.contentMode = .scaleAspectFit
 
         
-//        configureStackView()
 
     
 }
+    
+//    func configureStackView() {
+//        view.addSubview(stackView!)
+//        stackView.axis = .horizontal
+//        stackView.distribution = .fillEqually
+//        stackView.spacing = 20
+//
+//
+//        addButtonToStackView()
+//        setStackViewConstraints()
+//
+//    }
+    
+//    func addButtonToStackView() {
+//        let numberOfButton = 2
+//
+//        for i in 1...numberOfButton {
+//            let button = UIButton()
+//            button.setTitle("\(i)", for: .normal)
+//            stackView.addArrangedSubview(button)
+//        }
+//    }
+        
+        
+        
+//        //constraintes
+//        func setStackViewConstraints() {
+//            stackView.translatesAutoresizingMaskIntoConstraints = false
+//            stackView.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 20).isActive = true
+//            stackView.leadingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor, constant: 50).isActive = true
+//            stackView.trailingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor, constant: -50).isActive = true
+//            stackView.bottomAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.bottomAnchor, constant: -30).isActive = true
+//
+//
+//        }
+
 
 
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
